c9a4727b91f72ebbd9aee4ca8a48c279545dc5e0
B3D346 <115026292+B3D346@users.noreply.github.com>
2023-04-29 20:29:00 +0500

    modified:   commands/venox/timer.js

--- commands/venox/timer.js
+++ commands/venox/timer.js
@@ -9,21 +9,15 @@ module.exports = {
     await interaction.deleteReply();
 
     await interaction.channel.send("Started interval: 24s");
-
-     let dayend = setTimeout(Boolean(1), 24000);
-
-    do
-    {
-        if(dayend == '1')
-            {
-                interaction.channel.send("Interval completed.")
-            }
-    }
-    while(dayend != '1')
-    
+    const start = Date.now()
+     setTimeout(function()
+        {
+        const end = Date.now();
+        interaction.channel.send(`Total time passed:${(end - start)/1000}s`)
+        }, 24000);
 
     
-     
 
   },
-};
+
+}
