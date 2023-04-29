a884b0bf7ff2ec405a329e3b3984d2bbc1c11d1d
B3D346 <115026292+B3D346@users.noreply.github.com>
2023-04-29 20:01:22 +0500

    modified:   commands/venox/timer.js

--- commands/venox/timer.js
+++ commands/venox/timer.js
@@ -4,11 +4,11 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('timer')
     .setDescription('Test command to set required time'),
-  async execute(interaction, message) {
+  async execute(interaction) {
     await interaction.deferReply();
     await interaction.deleteReply();
 
-    await message.channel.send("Started interval: 12s & 48s");
+    await interaction.channel.send("Started interval: 12s & 48s");
 
   },
 };
