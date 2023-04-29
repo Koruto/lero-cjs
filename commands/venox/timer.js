7463f3188f935397adfb48106657495143175ed4
B3D346 <115026292+B3D346@users.noreply.github.com>
2023-04-29 21:21:06 +0500

    modified:   commands/venox/timer.js

--- commands/venox/timer.js
+++ commands/venox/timer.js
@@ -1,11 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, WebhookClient } = require('discord.js');
+const townhook = new WebhookClient({ id: '1101903776744484994', token: 'R9Sujh4KS8b1zxAq6yW10B0BFd4HuNdZhYbq24QU-uLGs-k_wQk0JBv7KMKxxoy1STEU' });
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('timer')
     .setDescription('Test command to set required time'),
-  async execute(interaction, client) {
-    await interaction.deferReply();
+  async execute(interaction, message) {
     await interaction.reply("Started interval: 5s");
 
     const start = Date.now()
@@ -13,8 +13,13 @@ module.exports = {
      setTimeout(function()
         {
         const end = Date.now();
-        interaction.channel.send(`Total time passed:${(end - start)/1000}s`)
-        client.channels.cache.get('1100799293079691394').send(`Alternative send method`)
+
+
+        townhook.send({
+            content: `Total time passed:${(end - start)/1000}s`,
+            username: 'Lero AI',
+            avatarURL: 'https://cdn.discordapp.com/avatars/1095648453721333790/70e5059d02f5c7a10733a9d4bc69e0ae.webp',
+        });
         }, 5000);
 
   },
