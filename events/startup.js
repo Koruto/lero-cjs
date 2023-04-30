6f87db3ae430c10aec78ac924551d6883a07b540
B3D346 <115026292+B3D346@users.noreply.github.com>
2023-04-30 13:00:57 +0500

    modified:   events/startup.js

--- events/startup.js
+++ events/startup.js
@@ -1,11 +1,14 @@
 const { Events } = require('discord.js');
-const { sleep } = require('system-sleep')
+const { sleep } = require('system-sleep');
+const execSync = require('child_process').execSync;
+
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client,interaction) {
-    sleep(3000);
-    console.log(`Running startup script...`);
+    sleep(3000);    // adds 3 second delay to run 
 
+    const output = execSync('echo Hello World', { encoding: 'utf-8' }); // the command to run, stores command output in "output"
+		console.log('Running Startup script...\n', output);     // logs the output to console
   },
 };
