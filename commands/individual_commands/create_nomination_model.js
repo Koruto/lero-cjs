f60ebd94986b636c1c6a437e497949857ba66f4d
Koru <flyingshadow03@gmail.com>
2024-06-24 21:02:33 +0530

    Added missing import

--- commands/individual_commands/create_nomination_model.js
+++ commands/individual_commands/create_nomination_model.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const { define_Nomination } = require('../../models/nomination');
 const { define_History } = require('../../models/history');
 const { define_Game } = require('../../models/gameConstants');
+const { define_Rooms } = require('../../models/rooms');
 const { Game } = require('../../util/constants');
 
 const data = new SlashCommandBuilder()
