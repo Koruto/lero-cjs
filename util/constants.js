6b3bfd9bb5716d98970e5b2dd86eb39b0147e82b
Koru <flyingshadow03@gmail.com>
2024-06-28 10:36:04 +0530

    Feature: Added selecting server from .env

--- util/constants.js
+++ util/constants.js
@@ -20,50 +20,50 @@ const ListOfCommands = {
 
 // For Koruto Server
 
-// const Game = {
-//   playingId: '1101087801925181485',
-//   noVoteId: '1101602215212372179',
-//   pendingId: '1107011779776761999',
-//   aliveId: '1101859415767924930',
-//   deadId: '1101602543454408764',
-//   spectatorId: '1109510808397951016',
-//   twelveHoursInSeconds: 43200,
-//   townSquareRole: '1100061376694722693',
-//   categoryId: '1100059641498574949',
-//   archivedCategoryId: '1100059677380849774',
-//   guildId: '1100058607099318354',
-//   viewChannelPermission: '0x0000000000000400',
-//   sendMessagePermission: '0x0000000000000800',
-//   validRooms: [
-//     'blacksmith-shop_',
-//     'infirmary_',
-//     'tavern_',
-//     'main-garden_',
-//     'edge-of-town_',
-//     'train-station_',
-//     'alley_',
-//     'inn_',
-//     'sheriff-station_',
-//     'carpentry-shop_',
-//   ],
-//   placeRoles: {
-//     'Blacksmith Shop': '1100061352636186675',
-//     Infirmary: '1100061354901123153',
-//     Tavern: '1100061357182812311',
-//     'Main Garden': '1100061359988813934',
-//     'Edge Of Town': '1100061361951752225',
-//     'Train Station': '1100061364615135242',
-//     Alley: '1100061366733258824',
-//     Inn: '1100061369170149487',
-//     'Sheriff Station': '1100061371598647446',
-//     'Carpentry Shop': '1100061374241058870',
-//     'Town Square': '1100061376694722693',
-//   },
-// };
+const korutoServer = {
+  playingId: '1101087801925181485',
+  noVoteId: '1101602215212372179',
+  pendingId: '1107011779776761999',
+  aliveId: '1101859415767924930',
+  deadId: '1101602543454408764',
+  spectatorId: '1109510808397951016',
+  twelveHoursInSeconds: 43200,
+  townSquareRole: '1100061376694722693',
+  categoryId: '1100059641498574949',
+  archivedCategoryId: '1100059677380849774',
+  guildId: '1100058607099318354',
+  viewChannelPermission: '0x0000000000000400',
+  sendMessagePermission: '0x0000000000000800',
+  validRooms: [
+    'blacksmith-shop_',
+    'infirmary_',
+    'tavern_',
+    'main-garden_',
+    'edge-of-town_',
+    'train-station_',
+    'alley_',
+    'inn_',
+    'sheriff-station_',
+    'carpentry-shop_',
+  ],
+  placeRoles: {
+    'Blacksmith Shop': '1100061352636186675',
+    Infirmary: '1100061354901123153',
+    Tavern: '1100061357182812311',
+    'Main Garden': '1100061359988813934',
+    'Edge Of Town': '1100061361951752225',
+    'Train Station': '1100061364615135242',
+    Alley: '1100061366733258824',
+    Inn: '1100061369170149487',
+    'Sheriff Station': '1100061371598647446',
+    'Carpentry Shop': '1100061374241058870',
+    'Town Square': '1100061376694722693',
+  },
+};
 
 // For Ele's Server
 
-const Game = {
+const eleServer = {
   playingId: '1108450948306698240',
   noVoteId: '1078003254807506954',
   pendingId: '1082346141003296871',
@@ -104,51 +104,6 @@ const Game = {
   },
 };
 
-// For SilentSand Server
-
-// const Game = {
-//   playingId: '1186459215418626130',
-//   noVoteId: '1187740478637101106',
-//   pendingId: '1187740594945138768',
-//   aliveId: '1187733992041103441',
-//   deadId: '1186459429344907305',
-//   spectatorId: '1187732853312729119',
-//   twelveHoursInSeconds: 43200,
-//   townSquareRole: '1187732942450069607',
-//   categoryId: '1187770983298371705',
-//   archivedCategoryId: '1187740296214220810',
-//   guildId: '1100058607099318354',
-//   viewChannelPermission: '0x0000000000000400',
-//   sendMessagePermission: '0x0000000000000800',
-//   validRooms: [
-//     'starbaker-show_',
-//     'radiance_',
-//     'planetside_',
-//     'the-crystal-ball_',
-//     'akatsuki-headquarters_',
-//     'ring-of-spears_',
-//     'secret-council_',
-//     'underground_',
-//     'forum-posts_',
-//     'terms-of-services_',
-//     'train-station_',
-//   ],
-//   placeRoles: {
-//     'Starbaker Show': '1187733144586166282',
-//     Radiance: '1187733286118764595',
-//     Planetside: '1187733319434117191',
-//     'The Crystal Ball': '1187733382877159454',
-//     'Akatsuki Headquarters': '1187733417371115550',
-//     'Ring of Spears': '1187733489387307109',
-//     'Secret Council': '1187733528360779866',
-//     Underground: '1187733608404881471',
-//     'Forum Posts': '1187733656349974559',
-//     'Terms of Services': '1187733696363634708',
-//     'Train Station': '1187733761748643891',
-//     'Town Square': '1187732942450069607',
-//   },
-// };
-
 async function define_Variables() {
   let timeOfDay = {
     currentDay: 1,
@@ -163,8 +118,27 @@ async function define_Variables() {
   return timeOfDay;
 }
 
+const getGameConfig = () => {
+  const server = process.env.SERVER;
+
+  console.log(`Server configuration selected: "${server || 'Ele (default)'}"`);
+
+  if (!server) {
+    return eleServer;
+  }
+
+  switch (server.toLowerCase()) {
+    case 'koruto':
+      return korutoServer;
+    case 'ele':
+      return eleServer;
+    default:
+      return eleServer;
+  }
+};
+
 module.exports = {
-  Game,
+  Game: getGameConfig(),
   ROOM_LIMIT,
   define_Variables,
   messageConstants,
